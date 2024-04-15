ca37aae0c1e7670d99f8d4ee5ea3a5d78575666f
shijianzhong <994129509@qq.com>
2024-04-15 17:43:22 +0800

    20240415

--- src/utils/views/in-memory.ts
+++ src/utils/views/in-memory.ts
@@ -1,6 +1,6 @@
 // import type { APIRoute } from 'astro'
 import { createClient } from '@vercel/kv'
-import console from 'console';
+// import console from 'console';
 
 
 // const KV_REST_API_URL = import.meta.env.KV_REST_API_URL
@@ -11,13 +11,9 @@ const KV_URL = process.env.KV_URL;
 const KV_REST_API_URL = process.env.KV_REST_API_URL;
 const KV_REST_API_TOKEN = process.env.KV_REST_API_TOKEN;
 const KV_REST_API_READ_ONLY_TOKEN = process.env.KV_REST_API_READ_ONLY_TOKEN;
-console.log("*************")
-console.log("KV_URL",KV_URL)
-// console.log("KV_REST_API_URL",KV_REST_API_URL)
-// const KV_URL = "redis://default:bc355eff752449b18b3276087684f5a1@champion-toad-34071.upstash.io:34071"
-// const KV_REST_API_URL = "https://champion-toad-34071.upstash.io"
-// const KV_REST_API_TOKEN = "AYUXASQgZDU2MGQxMjYtNWI5Yi00NDMwLThhYmUtMjcwYjM4YTk3MzZlYmMzNTVlZmY3NTI0NDliMThiMzI3NjA4NzY4NGY1YTE="
-// const KV_REST_API_READ_ONLY_TOKEN = "AoUXASQgZDU2MGQxMjYtNWI5Yi00NDMwLThhYmUtMjcwYjM4YTk3MzZl0Ovx8BoEWCUk2Kwnqc_IMTUn7h8lChO6Xtagw86RTck="
+// console.log("*************")
+// console.log("KV_URL",KV_URL)
+
 const client = new Map<string, number>();
 
 
