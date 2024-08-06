fc6f98ededfa1bd89bf4315fc4aa9f6258cf03e3
shijianzhong <994129509@qq.com>
2024-08-06 21:36:13 +0800

    telegram

---
+++ src/pages/static/[...url].js
@@ -0,0 +1,22 @@
+const targetWhitelist = [
+  't.me',
+  'telegram.org',
+  'telegram.me',
+  'telegram.dog',
+  'cdn-telegram.org',
+]
+
+export const prerender = false
+
+export async function GET({ request, params, url }) {
+  try {
+    const target = new URL(params.url + url.search)
+    if (!targetWhitelist.some(domain => target.hostname.endsWith(domain))) {
+      return Response.redirect(target.toString(), 302)
+    }
+    return fetch(target.toString(), request)
+  }
+  catch (error) {
+    return new Response(error.message, { status: 500 })
+  }
+}
