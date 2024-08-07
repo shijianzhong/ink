67ae725511f76d44834ab1b147beab607b4b12a3
shijianzhong <994129509@qq.com>
2024-08-07 20:43:52 +0800

    ink-home proxy tg

--- src/lib/telegram/index.js
+++ src/lib/telegram/index.js
@@ -111,7 +111,7 @@ export async function getChannelInfo(Astro, { before = '', after = '', q = '', t
   }
 
   // Where t.me can also be telegram.me, telegram.dog
-  const host = getEnv(import.meta.env, Astro, 'HOST') ?? 't.me'
+  const host = getEnv(import.meta.env, Astro, 'HOST') ?? 'tg.ink-home.cn'   //'t.me'
   const channel = getEnv(import.meta.env, Astro, 'CHANNEL')
   const staticProxy = getEnv(import.meta.env, Astro, 'STATIC_PROXY') ?? '/static/'
 
